0ccc908bd5362693f13bd95cc31926a0964d044b
Kevin Hughes <kevin.hughes@shopify.com>
2015-11-07 11:57:50 +0200

    generate shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.api_key = "2ddc34b467cde2cf83e250c6e02879b2"
+  config.secret = "f8174f58f6b3e255aaa4843faa0274a8"
+  config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
