44f4abaded501f4d2bbde68034312fe92fb5873c
Kevin Hughes <kevin.hughes@shopify.com>
2015-11-07 17:29:44 +0200

    prefer load creds from env

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
-  config.api_key = "2ddc34b467cde2cf83e250c6e02879b2"
-  config.secret = "f8174f58f6b3e255aaa4843faa0274a8"
-  config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
+  config.api_key = ENV['SHOPIFY_API_KEY'] || "2ddc34b467cde2cf83e250c6e02879b2"
+  config.secret = ENV['SHOPIFY_SECRET'] || "f8174f58f6b3e255aaa4843faa0274a8"
+  config.redirect_uri = ENV['SHOPIFY_REDIRECT'] || "http://localhost:3000/auth/shopify/callback"
   config.scope = "write_products"
   config.embedded_app = true
 end
