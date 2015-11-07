2c5d958e7ce6b85ce66fddcc6ea84289095603fe
Kevin Hughes <kevin.hughes@shopify.com>
2015-11-07 17:44:24 +0200

    update migration

--- db/migrate/20151107123457_add_default_to_since_id_on_tweet_data.rb
+++ db/migrate/20151107123457_add_default_to_since_id_on_tweet_data.rb
@@ -1,5 +1,5 @@
 class AddDefaultToSinceIdOnTweetData < ActiveRecord::Migration
   def change
-    change_column :tweet_data, :since_id, :integer, default: 1, limit: 20
+    change_column :tweet_data, :since_id, :bigint, default: 1
   end
 end
